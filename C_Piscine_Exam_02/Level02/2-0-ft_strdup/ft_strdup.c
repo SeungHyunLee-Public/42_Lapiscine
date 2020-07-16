1bab89852b945bd3fb8cb757b5181960f003bab3
seunghyl <lutious2@gmail.com>
2020-07-16 22:59:26 +0900

    c06

---
+++ C_Piscine_Exam_02/Level02/2-0-ft_strdup/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/06 18:22:00 by angavrel          #+#    #+#             */
+/*   Updated: 2020/07/16 21:44:48 by seunghyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*s;
+	int		len;
+
+	while (src[len])
+		++len;
+	if (!(s = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	s[len] = '\0';
+	while (len >= 0)
+	{
+		s[len] = src[len];
+		len--;
+	}
+	return (s);
+}
---
+++ C_Piscine_Exam_02/Level02/2-1-max/max.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   max.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/06 15:30:33 by angavrel          #+#    #+#             */
+/*   Updated: 2020/07/16 15:28:48 by seunghyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>//
+#include <stdlib.h>//
+
+int		max(int *tab, unsigned int len)
+{
+	int	max;
+
+	if (!len)
+		return (0);
+	max = tab[--len];
+	while (len--)
+		if (tab[len] > max)
+			max = tab[len];
+	return (max);
+}
+
+int		main(void)//
+{//
+	int	*tab;//
+
+	if (!(tab = (int*)malloc(sizeof(int) * 3)))//
+		return (0);//
+	tab[0] = 7;//
+	tab[1] = 6;//
+	tab[2] = 4;//
+	printf("%d", max(tab, 3));//
+	return (0);//
+}//
